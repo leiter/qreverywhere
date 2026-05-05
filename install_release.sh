d29b903665586de18623700338218d8197d011f6
marcoleiter <mmaleiter@gmail.com>
2026-05-05 16:21:29 +0200

    cleaning

--- install_release.sh
+++ install_release.sh
@@ -5,12 +5,6 @@ if [ ! -f "$BUNDLETOOL" ]; then
   echo "Downloading bundletool..."
   curl -L -o "$BUNDLETOOL" https://github.com/google/bundletool/releases/download/1.18.1/bundletool-all-1.18.1.jar
 fi
-AAB=androidApp/build/outputs/bundle/release/androidApp-release.aab
-APKS=/tmp/qreverywhere.apks
-KEYSTORE=/home/mandroid/Videos/AA_FILES/qreverywhere_signature.jks
-KS_PASS=vLr2hHCxTjNfQSv4
-KEY_ALIAS=qreverywhere
-KEY_PASS=vLr2hHCxTjNfQSv4
 
 set -e
 
